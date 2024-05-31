e616a8ac27036a32b236ddcde1fbebc0a0fdeec2
Damien MARTINS GOMES <damien.martins-gomes@ipsa.fr>
2024-05-31 09:27:40 -0400

    Fixing bugs

--- Image_Classification/src/AdaFisher.sh
+++ Image_Classification/src/AdaFisher.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # ADD YOUR SETTINGS HERE
-python train.py --config configs/AdaFisherCNN.yaml --pretrained # you can change on the yaml file for AdaFisherW
-# python train.py --config configs/AdaFisherViT.yaml # you can change on the yaml file for AdaFisherW
+python train.py --config configs/AdaFisherCNN.yaml # you can change on the yaml file for AdaFisherW
